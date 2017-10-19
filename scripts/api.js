d6fec726f46f61132155e2806a83b9f64c760f7f
FliiFe <theophile.cailliau@gmail.com>
2017-10-19 19:05:59 +0200

    More tolerant by-uri/ endpoint

--- scripts/api.js
+++ scripts/api.js
@@ -6,6 +6,26 @@ const path = require('path');
 const devicons = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../devicon/devicon.json')));
 
 const sanitize = title => title.match(/[a-z ]/gi).join('').replace(/ /g, '-').toLowerCase();
+const levenshtein = (a, b) => {
+    let t = [];
+    let m = a.length;
+    let n = b.length;
+    if (!m) return n;
+    if (!n) return m;
+    for (let j = 0; j <= n; j++) {
+        t[j] = j;
+    }
+    let u;
+    for (let i = 1; i <= m; i++) {
+        u = [i];
+        for (let j = 1; j <= n; j++) {
+            u[j] = a[i - 1] === b[j - 1] ? t[j - 1] : Math.min(t[j - 1], t[j], u[j - 1]) + 1;
+        }
+        t = u;
+    }
+    return u[n];
+};
+
 
 hexo.extend.filter.register('server_middleware', (app) => {
     app.use(hexo.config.root + 'api/', bodyParser.json({
@@ -51,7 +71,13 @@ hexo.extend.filter.register('server_middleware', (app) => {
         res.end(
             JSON.stringify(
                 hexo.model('Post').toArray()
-                    .filter(post => post._id.endsWith(req.body.id_suffix) && sanitize(post.title) === req.body.sanitized_title)
+                    .filter(post => post._id.endsWith(req.body.id_suffix))
+                    .map(post => ({
+                        ...post,
+                        sanitized: sanitize(post.title),
+                        levenshtein: levenshtein(req.body.sanitized_title, sanitize(post.title))
+                    }))
+                    .sort((a, b) => a.levenshtein-b.levenshtein)
                     .map(
                         post => ({
                             title: post.title,
@@ -69,7 +95,7 @@ hexo.extend.filter.register('server_middleware', (app) => {
     app.use(hexo.config.root + 'api/icon/by-language/', (req, res) => {
         const language = devicons.find(e => e.name === req.url.slice(1));
         const variation = (language && language.versions.svg[0]);
-        if(!variation) {
+        if (!variation) {
             res.statusCode = 404;
             res.end('');
             return;
